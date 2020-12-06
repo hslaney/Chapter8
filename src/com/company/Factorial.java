c7df4ce0ae1fae90c2ff9ea7aaed4cf6f13fcd25
hslaney <haley.slaney@gmail.com>
2020-12-06 15:07:28 -0500

    initial commit

--- src/com/company/Factorial.java
+++ src/com/company/Factorial.java
@@ -2,65 +2,96 @@ package com.company;
 
 public class Factorial {
 
-    public int calcFactorial(int x){
+    public long calcFactorial(int x){
 
-        int sum=1;
+        long sum = 1;
         for (int y = x; y>0; y--){
             sum *= y;
         }
         return sum;
 
-//        int sum = 1;
-//        while (x>0){
-//            sum *=x;
-//            x--;
-//        }
-//        return sum;
-
     }
 
-    public void calcE()
+    public double calcE()
     {
         double e = 1;
-        double old = 0;
+        double olde = 0;
         int counter = 1;
-        while (e-old > 0.001){
-            old=e;
+        while (e-olde > 0.001){
+            olde=e;
             e+= 1.0/(calcFactorial(counter));
             counter++;
-            System.out.println("e:" + e + "    old:" + old);
         }
+        return e;
     }
 
+    public double calcEX(int x){
+        double e = 1;
+        double olde = 0;
+        int counter = 1;
+        while (e-olde > 0.001){
+            olde=e;
+            double y = Math.pow(x, counter);
+            e+= y/(calcFactorial(counter));
+            counter++;
 
+        }
+        return e;
 
-
-
-
-
-
-
+    }
 
 
     public static void main (String args[]){
         Factorial f1 = new Factorial();
 
-//        System.out.println(f1.calcFactorial(1));
-//        System.out.println(f1.calcFactorial(2));
-//        System.out.println(f1.calcFactorial(3));
-//        System.out.println(f1.calcFactorial(4));
-//        System.out.println(f1.calcFactorial(5));
-//        System.out.println(f1.calcFactorial(6));
-//        System.out.println(f1.calcFactorial(7));
-//        System.out.println(f1.calcFactorial(8));
-//        System.out.println(f1.calcFactorial(9));
-//        System.out.println(f1.calcFactorial(10));
-//        System.out.println(f1.calcFactorial(19));
-//        System.out.println(f1.calcFactorial(20));
+        for (int x = 1; x<=20 ; x++)
+        {
+            System.out.println(x + "! = " + f1.calcFactorial(x));
+        }
 
-        f1.calcE();
+        System.out.println();
 
+        System.out.println("e is " + f1.calcE());
+        System.out.printf("rounded : %2.3f \n", f1.calcE());
 
+        System.out.println();
 
+        for (int x=1; x<=5; x++)
+        {
+            System.out.printf("e^" + x + " = %2.3f \n", f1.calcEX(x));
+        }
     }
 }
+
+/*
+output:
+1! = 1
+2! = 2
+3! = 6
+4! = 24
+5! = 120
+6! = 720
+7! = 5040
+8! = 40320
+9! = 362880
+10! = 3628800
+11! = 39916800
+12! = 479001600
+13! = 6227020800
+14! = 87178291200
+15! = 1307674368000
+16! = 20922789888000
+17! = 355687428096000
+18! = 6402373705728000
+19! = 121645100408832000
+20! = 2432902008176640000
+
+e is 2.7182539682539684
+rounded : 2.718
+
+e^1 = 2.718
+e^2 = 7.389
+e^3 = 20.085
+e^4 = 54.598
+e^5 = 148.413
+ */
